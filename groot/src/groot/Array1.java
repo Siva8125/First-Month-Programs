275e6453c59c133b185af8171a7360cce5681f36
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-09-06 12:28:10 +0530

    Array1

---
+++ groot/src/groot/Array1.java
@@ -0,0 +1,20 @@
+package groot;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		int a[]=new int[5];//Declaration and instantiation
+		a[0]=10; //initialization
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+		
+	}
+
+}
