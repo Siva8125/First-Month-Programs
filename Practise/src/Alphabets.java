1f4fd3031628c6dd36ac7014d38b9f8ea23b7519
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-09-07 10:26:01 +0530

    alphabets

---
+++ Practise/src/Alphabets.java
@@ -0,0 +1,16 @@
+
+public class Alphabets {
+
+	public static void main(String[] args) {
+		char c;
+		for(c='a';c<='z';c++) {
+			System.out.print(c+" ");
+		}
+		for(c='A';c<='Z';c++) {
+			System.out.print(c+" ");
+		}
+		
+		
+	}
+
+}
