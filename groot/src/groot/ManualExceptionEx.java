32dbd4c8c755f81b2d97b1a48f9e467cc67bcc6a
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-09-14 14:18:52 +0530

    prac

---
+++ groot/src/groot/ManualExceptionEx.java
@@ -0,0 +1,12 @@
+package groot;
+
+public class ManualExceptionEx {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	      NullPointerException nullPointer = new NullPointerException();
+	      throw nullPointer;
+	      System.out.println("How are you");
+	}
+
+}
---
+++ groot/src/groot/SuperclassUnchecked.java
@@ -0,0 +1,14 @@
+package groot;
+
+public class SuperclassUnchecked {
+
+	public static void main(String[] args) {
+        int[] a = new int[] { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < 6; i++) {
+        	
+            
+            System.out.println(a[i]);
+        }
+	}
+
+}
