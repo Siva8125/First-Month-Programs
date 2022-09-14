a4e7af2ceacc029d8e131070a505476e3a5fbb33
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-09-14 14:40:54 +0530

    time

---
+++ Practise/src/Countdigits.java
@@ -0,0 +1,12 @@
+
+public class Countdigits {
+
+	public static void main(String[] args) {
+		int count = 0;
+		int number = 000445;
+		for(;number !=0;number /= 10,count++) {
+	}
+		System.out.println("Number of digits :"+count);
+	}
+
+}
