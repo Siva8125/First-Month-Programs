bd8e7e5773ccdb72ed43c3584866e5d6a8268f7e
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-10-14 20:05:09 +0530

    lambda

---
+++ groot/src/Java8Features/LambdaForEach.java
@@ -0,0 +1,20 @@
+package Java8Features;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaForEach {
+	public static void main(String[] args) {  
+        
+        List<String> list=new ArrayList<String>();  
+        list.add("ankit");  
+        list.add("mayank");  
+        list.add("irfan");  
+        list.add("jai");  
+          
+        list.forEach(  
+            (n)->System.out.println(n)  
+        );  
+    }  
+
+}
