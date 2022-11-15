17c0ff45a4067af43e85cf5a3b2d5c014c7095c3
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2022-11-15 16:06:49 +0530

    ll

---
+++ Practise/src/AssignmentOperatorEx.java
@@ -0,0 +1,12 @@
+/*Java assignment operator is one of the most common operators. It is used to assign the value on its right to the operand on its left.*/
+public class AssignmentOperatorEx {
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 20;
+		a += 4;// a=a+4 (a=10+4)
+		b -= 4;// b=b-4 (b=20-4)
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
