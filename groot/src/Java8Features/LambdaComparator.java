d0c407fb8243f64398e3d0fed004285b2296abfe
Siva8125 <sivakumardamerla.vtalent@gmail.com>
2023-01-06 15:47:02 +0530

    fghfgh

--- groot/src/Java8Features/LambdaComparator.java
+++ groot/src/Java8Features/LambdaComparator.java
@@ -2,11 +2,11 @@ package Java8Features;
 import java.util.ArrayList;  
 import java.util.Collections;  
 import java.util.List;  
-class Product{  
+class Product3{  
     int id;  
     String name;  
     float price;  
-    public Product(int id, String name, float price) {  
+    public Product3(int id, String name, float price) {  
         super();  
         this.id = id;  
         this.name = name;  
@@ -16,12 +16,12 @@ class Product{
 
 public class LambdaComparator {
 	public static void main(String[] args) {  
-        List<Product> list=new ArrayList<Product>();  
+        List<Product3> list=new ArrayList<Product3>();  
           
         //Adding Products  
-        list.add(new Product(1,"HP Laptop",25000f));  
-        list.add(new Product(3,"Keyboard",300f));  
-        list.add(new Product(2,"Dell Mouse",150f));  
+        list.add(new Product3(1,"HP Laptop",25000f));  
+        list.add(new Product3(3,"Keyboard",300f));  
+        list.add(new Product3(2,"Dell Mouse",150f));  
           
         System.out.println("Sorting on the basis of name...");  
   
@@ -29,7 +29,7 @@ public class LambdaComparator {
         Collections.sort(list,(p1,p2)->{  
         return p1.name.compareTo(p2.name);  
         });  
-        for(Product p:list){  
+        for(Product3 p:list){  
             System.out.println(p.id+" "+p.name+" "+p.price);  
         }  
   
--- groot/src/Java8Features/StreamEx1.java
+++ groot/src/Java8Features/StreamEx1.java
@@ -3,12 +3,12 @@ package Java8Features;
 import java.util.*;
 import java.util.stream.Collectors;
 
-class Product {
+class Product2 {
 	int id;
 	String name;
 	float price;
 
-	public Product(int id, String name, float price) {
+	public Product2(int id, String name, float price) {
 		this.id = id;
 		this.name = name;
 		this.price = price;
@@ -17,13 +17,13 @@ class Product {
 
 public class StreamEx1 {
 	public static void main(String[] args) {
-		List<Product> productsList = new ArrayList<Product>();
+		List<Product2> productsList = new ArrayList<Product2>();
 		// Adding Products
-		productsList.add(new Product(1, "HP Laptop", 25000f));
-		productsList.add(new Product(2, "Dell Laptop", 30000f));
-		productsList.add(new Product(3, "Lenevo Laptop", 28000f));
-		productsList.add(new Product(4, "Sony Laptop", 28000f));
-		productsList.add(new Product(5, "Apple Laptop", 90000f));
+		productsList.add(new Product2(1, "HP Laptop", 25000f));
+		productsList.add(new Product2(2, "Dell Laptop", 30000f));
+		productsList.add(new Product2(3, "Lenevo Laptop", 28000f));
+		productsList.add(new Product2(4, "Sony Laptop", 28000f));
+		productsList.add(new Product2(5, "Apple Laptop", 90000f));
 		List<Float> productPriceList2 = productsList.stream().filter(p -> p.price > 30000)// filtering data
 				.map(p -> p.price) // fetching price
 				.collect(Collectors.toList()); // collecting as list
